df29fb890b07d5957b406c393de4357adf022e3a
tianyihang <xarzhi@noreply.gitcode.com>
2025-07-21 11:50:18 +0800

    up

--- packages/components/tree/src/tree.vue
+++ packages/components/tree/src/tree.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="t-tree">
-    <t-tree-node v-for="node in data" :key="node.id" :node="node" @handleClickNode="emit('handleClickNode',$event)"</t-tree-node>
+    <t-tree-node
+      v-for="node in data"
+      :key="node.id"
+      :node="node"
+      @handleClickNode="emit('handleClickNode', $event)"
+    ></t-tree-node>
   </div>
 </template>
 
 <script setup>
-import { TreeProps ,TreeNodeEmit} from './tree';
-import tTreeNode from './tree-node.vue'
+import { TreeProps, TreeNodeEmit } from "./tree";
+import tTreeNode from "./tree-node.vue";
 
-const props=defineProps(TreeProps)
-const emit = defineEmits(TreeNodeEmit)
+const props = defineProps(TreeProps);
+const emit = defineEmits(TreeNodeEmit);
 
 defineOptions({
-  name:'t-tree'
-})
+  name: "t-tree",
+});
 </script>
